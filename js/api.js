0fa01bdb2f0c60775e2f3cdebe468d9f704c2b49
ravenusmc <mcuddy77@gmail.com>
2016-05-15 15:59:18 -0400

    modified JS file

--- js/api.js
+++ js/api.js
@@ -10,10 +10,14 @@ var request = {
         "income",
         "population",
         "median_male_age",
+        "median_home_value",
+        "poverty"
     ],
-    //"api": "acs5",
+    "api": "acs5",
     "year": "2013"
 };
+
+alert(request.variables[1])
 
 census.APIRequest(request, function (response) {
     //Outputs the raw JSON text
@@ -22,54 +26,5 @@ census.APIRequest(request, function (response) {
 
 
 
-
-
-
-
-//// OLD BELOW HERE IGNORE IGNORE  /////////
-// var sdk = new CitySDK();
-// census = sdk.modules.census;
-// census.enable("4eec81c83b1659c8430d700582947782ab1c196c");
-
-// var request = {
-//     "zip": "30519",
-//     "variables": [
-//         "income",
-//         "population",
-//         "median_male_age",
-//     ],
-//     "level": "tract"
-// }
-
-// var callback = function(response) {
-//     map.data.addGeoJson(response);
-// };
-
-// census.GEORequest(request, callback);
-
-/////////////// Version 2 ////////////////////
-
-//demographics of Gwinnett county 
-
-// var sdk = new CitySDK();
-// census = sdk.modules.census;
-// census.enable("4eec81c83b1659c8430d700582947782ab1c196c");
-
-// var request = {
-//     "level":"state",
-//     "state":"GA",
-//     "sublevel":"county",
-//     "name": "Gwinnett County",
-//     "variables": [
-//         "income",
-//         "population",
-//         "median_male_age",
-//     ],
-//     "year": "2014",  
-// }
-
-// census.APIRequest(request, function(response) {
-//     jQuery("#data").text(JSON.stringify(response, null, 4));
-// });
 
 
